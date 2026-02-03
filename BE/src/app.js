7be3e07bdce1506dfcc7d4c37c48b0c2063f1b0a
hadimysuf <hadimuhammadyusuf019@gmail.com>
2026-02-04 03:40:44 +0700

    HADI: increase limit

--- BE/src/app.js
+++ BE/src/app.js
@@ -14,7 +14,8 @@ const swaggerDocument = require(path.join(__dirname, '..', 'swagger.json'));
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Simple request logger
 app.use((req, res, next) => {
